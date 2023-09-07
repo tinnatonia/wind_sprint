6a573617c999dd70122ae74ce5b2f8188f3b17e6
Julia Eager <eager.julia@gmail.com>
2023-09-07 11:52:16 -0400

    operators and alternative energy production 014

--- jme_sprint/code/operators_other_types_014.sql
+++ jme_sprint/code/operators_other_types_014.sql
@@ -1,5 +1,11 @@
---010 who owns which plants and assess the footprint of turbine OWNERS
-select t.eia_id eia, t.p_tnum, t.p_name project_name, t.p_year project_year, op.plant_name, op.plant_id, op.operator_name, op.operator_id, t.t_manu turbine_manu, t.t_model turbine_model, t.ylat latitude, t.xlong longitude
-from wind_turbine_20220114 t
-left join eia923_operators op on t.eia_id = op.plant_id
-where operator_id ilike '15399';
+--014 who operates the wind turbines in the USA, and whether they operate other types of power plants, to assess potential takeover targets
+
+	
+select plant_id, plant_name, operator_name, operator_id, plant_state, aer_fuel_type_code, (net_generation_megawatthours/1000) net_gen_mwh
+from eia923_operators
+where operator_id in(select distinct operator_id
+	from(
+	select t.eia_id, op.operator_id
+	from wind_turbine_20220114 t
+	left join eia923_operators op
+		on t.eia_id = op.plant_id) t1)
